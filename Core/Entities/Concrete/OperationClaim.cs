2a3a9b4387c2877021e0450a30f8ee58be11dfc1
Buğra Öçalan <bugraocalan@gmail.com>
2020-01-03 17:02:57 +0300

    Add project files.

---
+++ Core/Entities/Concrete/OperationClaim.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Entities.Concrete
+{
+    //Kullanici Rolleri
+  public class OperationClaim
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Core/Utilities/DbStatus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities
+{
+    public static class DbStatus
+    {
+        public enum DbStatusEnum
+        {
+           Deleted=2,
+           Updated=1,
+           Active=0,
+        }
+
+    }
+}
