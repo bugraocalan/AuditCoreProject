75e6995d636ea742845321207c1f1db963c5d17c
Buğra Öçalan <bugraocalan@gmail.com>
2020-01-06 16:35:18 +0300

    Business Katmanı Kodlaması yapıldı.

--- Core/Utilities/Results/ErrorResult.cs
+++ Core/Utilities/Results/ErrorResult.cs
@@ -4,7 +4,14 @@ using System.Text;
 
 namespace Core.Utilities.Results
 {
-    public class ErrorResult
+    public class ErrorResult : Result
     {
+        public ErrorResult(string message) : base(false, message)
+        {
+        }
+
+        public ErrorResult() : base(false)
+        {
+        }
     }
 }
