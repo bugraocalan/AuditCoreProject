4c41c6f3a9c55f2009722f1cd58954f2804f2bf4
bugra4569 <bugraocalan@gmail.com>
2020-01-11 22:45:13 +0300

    --

---
+++ Entities/Concrete/MasterTamplateSurvey.cs
@@ -0,0 +1,16 @@
+﻿using Core.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class MasterTamplateSurvey : IEntity
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int CreatedUser { get; set; }
+        public int Status { get; set; } 
+    }
+}
