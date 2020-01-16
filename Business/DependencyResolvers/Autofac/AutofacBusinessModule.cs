e4b733864552626a98dcd9021cee765831240c4a
bugra4569 <bugraocalan@gmail.com>
2020-01-16 21:38:49 +0300

    Autofac corrected

--- Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
+++ Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -23,6 +23,23 @@ namespace Business.DependencyResolvers.Autofac
             builder.RegisterType<UserManager>().As<IUserService>();
             builder.RegisterType<EfUserDal>().As<IUserDal>();
 
+            builder.RegisterType<AuditManager>().As<IAuditService>();
+
+            builder.RegisterType<CustomerManager>().As<ICustomerDal>();
+            builder.RegisterType<EfCustomerDal>().As<ICustomerDal>();
+
+            builder.RegisterType<SurveyDetailManager>().As<ISurveyDetailService>();
+            builder.RegisterType<EfSurveyDetailDal>().As<ISurveyDetailDal>();
+
+            builder.RegisterType<SurveyMasterManager>().As<ISurveyMasterService>();
+            builder.RegisterType<EfSurveyMasterDal>().As<ISurveyMasterDal>();
+
+            builder.RegisterType<TemplateSurveyDetailManager>().As<ITemplateSurveyDetailService>();
+            builder.RegisterType<EfTemplateSurveyDetailDal>().As<ITemplateSurveyDetailDal>();
+
+            builder.RegisterType<TemplateSurveyMasterManager>().As<ITemplateSurveyMasterDal>();
+            builder.RegisterType<EfTemplateSurveyMasterDal>().As<ITemplateSurveyMasterDal>();
+
             builder.RegisterType<AuthManager>().As<IAuthService>();
             builder.RegisterType<JwtHelper>().As<ITokenHelper>();
         }
